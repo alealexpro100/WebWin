7eceb2940f24ba834ce4626553bbd328adf21888
ALEXPRO100 <alealexn@gmail.com>
2022-05-28 21:27:17 +0800

    Add functions for plugins

--- web_root/assets/webwin.js
+++ web_root/assets/webwin.js
@@ -1,16 +1,81 @@
+var project_name="WebWin";
+
+var current_module_id;
+var current_module_name;
+
+async function sleep(time) {
+    return new Promise(resolve => setTimeout(resolve, time));
+}
+
+function ToastIcon(icon, text) {
+    M.toast({unsafeHTML: "<span class=\"material-icons\">"+icon+"</span>"+ text});
+}
+
+function ToastError(text) {
+    ToastIcon("error", "Error (see console): "+text);
+}
+
+async function actionGetStatus(action_id) {
+    var tmp;
+    await $.post("/api/plugins?action=get_status&id="+action_id, function(data, status) {
+        tmp=data;
+    }).fail(function(jqXHR, textStatus, error) {
+        ToastError(error);
+    });
+    return tmp;
+}
+
+async function actionRequest(module_id, action, param) {
+    var tmp;
+    await $.post("/api/plugins/"+module_id+"?action="+action+"&param="+param, function(data, status) {
+        tmp=data;
+    }).fail(function(jqXHR, textStatus, error) {
+        ToastError(error);
+    });
+    return tmp;
+}
+
+async function actionRequestAndWait(module_id, action, param) {
+    var id = await actionRequest(module_id, action, param);
+    var result = {"status": "pending"};
+    while (result.status == "pending") {
+        result = await actionGetStatus(id);
+        await sleep(400);
+    }
+    return result;
+}
+
+async function clearJobs() {
+    return await $.post("/api/plugins?action=jobs_clear", function(data, status){
+        console.log("Clear jobs success: " + data);
+    }).fail(function(jqXHR, textStatus, error) {
+        ToastError(error);
+    });
+}
+
 (function(e,t) {
     window.addEventListener('load', init);
-    var project_name="WebWin";
     var selected;
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Proxy
+    // Pretty fast way to get url params
+    const params = new Proxy(new URLSearchParams(window.location.search), {
+        get: (searchParams, prop) => searchParams.get(prop),
+      });
     function init() {
         M.AutoInit();
-        load_module("000_main", "Main page");
+        if (params.id==null) {
+            load_module("000_main", "Main page");
+        } else {
+            selected=params.id;
+            load_module(params.id, params.name);
+        }
         $('#logo-container').on("click", function() {
             jQuery(selected).removeClass("active");
             load_module("000_main", "Main page");
         });
         //Add menu buttons from plugins
         $.post("/api/plugins?action=list", function(data, status) {
+            //In fact, server returns string with json, so we need to parse it
             var obj = jQuery.parseJSON(data);
             jQuery.each( obj["plugins"], function( i, val ) {
                 if (val["hidden"])
@@ -20,32 +85,34 @@
                     .on("click", function() {
                         button_module(this);
                     });
-                $("#menu").append(el);
+                $('#menu').append(el);
             });
         });
     }
-    $.fn.ignore = function(sel) {
-        return this.clone().find(sel || ">*").remove().end();
-      };
     async function load_module(module_id, module_name) {
         return await $.get("plugins/"+module_id+"/index.html", function(data, status){
             $('title').attr("module_id",module_id).text(module_name.charAt(0).toUpperCase() + module_name.slice(1) +" - "+project_name);
             $('#page_name').text(module_name.charAt(0).toUpperCase() + module_name.slice(1));
             $('main #module_content').empty().append(data);
+            current_module_id=module_id;
+            current_module_name=module_name;
+            window.history.pushState({}, module_name, "?id="+module_id+"&name="+module_name);
             console.log("Loaded module: \""+module_id+"\".");
-            result=true;
         }).fail(function() {
             M.toast({text: "Failed to load plugin "+module_id+"!"});
         });
     }
     async function button_module(element) {
+        if (selected==element)
+            return;
         var module_id=jQuery(element).attr("module_id");
         // Code from here: https://stackoverflow.com/questions/11347779
+        // We ignore any other elements except text
         var module_name=jQuery(element).children("a").contents().filter(function() {return this.nodeType == 3;}).text();
         if (await load_module(module_id, module_name)) {
             jQuery(selected).removeClass("active");
             selected=element;
-            jQuery(element).addClass("active")
+            jQuery(element).addClass("active");
         }
     }
 })();
