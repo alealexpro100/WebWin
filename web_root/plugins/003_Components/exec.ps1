99d013fa25b7883b640d0a9ddaa0bc9a0e01f509
alealexpro100 <alealexn@gmail.com>
2022-05-30 05:01:08 -0700

    Make components plugin working

--- web_root/plugins/003_Components/exec.ps1
+++ web_root/plugins/003_Components/exec.ps1
@@ -2,18 +2,21 @@ $ErrorActionPreference = "Stop"
 # See https://stackoverflow.com/questions/9948517 for getting exitcode of normal exe's
 
 function GetFeatures() {
-    Get-WindowsFeature |  Where-Object {$_.Depth -eq 1 } | Select-Object -Property Name,DisplayName,Installed,DependsOn,Depth | ConvertTo-Json
+    Get-WindowsFeature |  Where-Object {$_.Depth -eq 1 } | Select-Object -Property DisplayName,Installed,Description,DependsOn,Name | ConvertTo-Json
 }
 
 function InstallFeature($name, $AllSub, $Manage) {
-    $options = {}
-    if ($AllSub -eq 1) {
-        $options += "-IncludeAllSubFeature"
+    if ($AllSub -eq "1") {
+        $AllSub = $true
+    } else {
+        $AllSub = $false
     }
-    if ($Manage -eq 1) {
-        $options += "-IncludeManagementTools"
+    if ($Manage -eq "1") {
+        $Manage = $true
+    } else {
+        $Manage = $false
     }
-    Install-WindowsFeature -Name Web-Server $options
+    Install-WindowsFeature -Name $name -IncludeAllSubFeature:$AllSub -IncludeManagementTools:$Manage
 }
 
 function UninstallFeature($name) {
@@ -25,8 +28,8 @@ $opts = $args[0].Split(",")
 # Normally you should only add or change functions.
 switch ( $opts[0] ) {
     "get" {GetFeatures}
-    "install" {InstallFeature}
-    "uninstall" {UninstallFeature}
+    "install" {InstallFeature $opts[1] $opts[2] $opts[3]}
+    "uninstall" {UninstallFeature $opts[1]}
 }
 
 [Environment]::Exit(0)
